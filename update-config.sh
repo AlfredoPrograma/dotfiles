313621718470d44425272b1d84184f70422782b2
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2024-08-15 19:24:36 -0400

    move files to top level dotfiles directory

--- update-config.sh
+++ update-config.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+# Remove old ./dotfiles dir
 rm -rf ./dotfiles
 
-BASE_CONFIG_DIR="$HOME/.config"
+# Recreate ./dotfiles dir
+mkdir dotfiles
 
+BASE_CONFIG_DIR="$HOME/.config"
 apps_configs_dirs=("i3" "alacritty" "nvim" "picom" "polybar" "rofi")
 
 
 for dir in "${apps_configs_dirs[@]}"
 	do
 		echo "Copying $dir"
-		cp -r "$BASE_CONFIG_DIR/$dir" "./$dir"
+		cp -r "$BASE_CONFIG_DIR/$dir" "./$dotfiles/$dir"
 	done
 
 
