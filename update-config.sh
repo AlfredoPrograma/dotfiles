c8d7e25f555f79b1ac7da1272f9aa170c7a159ce
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2024-08-15 19:31:07 -0400

    add saving screenshot as file keybind

--- update-config.sh
+++ update-config.sh
@@ -4,7 +4,7 @@
 rm -rf ./dotfiles
 
 # Recreate ./dotfiles dir
-mkdir dotfiles
+mkdir ./dotfiles
 
 BASE_CONFIG_DIR="$HOME/.config"
 apps_configs_dirs=("i3" "alacritty" "nvim" "picom" "polybar" "rofi")
@@ -13,7 +13,7 @@ apps_configs_dirs=("i3" "alacritty" "nvim" "picom" "polybar" "rofi")
 for dir in "${apps_configs_dirs[@]}"
 	do
 		echo "Copying $dir"
-		cp -r "$BASE_CONFIG_DIR/$dir" "./$dotfiles/$dir"
+		cp -r "$BASE_CONFIG_DIR/$dir" "./dotfiles/$dir"
 	done
 
 
